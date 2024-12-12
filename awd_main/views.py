40d68422fb99c0391f70f733d6441bee07536ac9
Illia_pc <anukovich2@gmail.com>
2024-12-12 22:02:36 +0100

    import data task celery created

--- awd_main/views.py
+++ awd_main/views.py
@@ -1,5 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from dataentry.tasks import celery_test_task
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def celery_test(request):
+    # for test execute a time consuming task here:
+    celery_test_task.delay()
+    return HttpResponse('<h3>Function execited successfully</h3>')
---
+++ dataentry/tasks.py
@@ -0,0 +1,18 @@
+from awd_main.celery import app
+import time 
+from django.core.management import call_command
+
+
+@app.task
+def celery_test_task():
+    time.sleep(10) # simulation of a time consuming task  
+    return 'Task executed successfully!'
+
+
+@app.task
+def import_data_task(file_path, model_name):
+    try:
+        call_command('importdata', file_path, model_name)
+    except Exception as e:
+        raise e
+    return 'Data imported successfully'
