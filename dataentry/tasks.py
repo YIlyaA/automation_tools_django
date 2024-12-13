ecbe9e54dec5ad1e04f9c8a3eb8039bd062fa507
Illia_pc <anukovich2@gmail.com>
2024-12-13 21:03:35 +0100

    Setting up email config; added .env file

--- dataentry/tasks.py
+++ dataentry/tasks.py
@@ -15,4 +15,6 @@ def import_data_task(file_path, model_name):
         call_command('importdata', file_path, model_name)
     except Exception as e:
         raise e
+    
+    # notify user by email
     return 'Data imported successfully'
