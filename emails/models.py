14fb7b18f5ec918d572c3702ac89be4528d8095b
unknown <anukovich2@gmail.com>
2024-12-17 13:42:52 +0100

    email tracking model, url patterns

--- emails/models.py
+++ emails/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 
+
 class List(models.Model):
     email_list = models.CharField(max_length=25)
 
     def __str__(self):
         return self.email_list
-    
+
 
 class Subscriber(models.Model):
     email_list = models.ForeignKey(List, on_delete=models.CASCADE)
@@ -14,14 +15,25 @@ class Subscriber(models.Model):
 
     def __str__(self):
         return self.email_address
-    
+
 
 class Email(models.Model):
     email_list = models.ForeignKey(List, on_delete=models.CASCADE)
     subject = models.CharField(max_length=100)
     body = RichTextField()
-    attachment = models.FileField(upload_to='email_attachments/', blank=True)
+    attachment = models.FileField(upload_to="email_attachments/", blank=True)
     sent_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.subject
+
+
+class EmailTracking(models.Model):
+    email = models.ForeignKey(Email, on_delete=models.CASCADE, null=True, blank=True)
+    subscriber = models.ForeignKey(Subscriber, on_delete=models.CASCADE, null=True, blank=True)
+    unique_id = models.CharField(max_length=255, unique=True)
+    opened_at = models.DateTimeField(null=True, blank=True)
+    clicked_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.email.subject
