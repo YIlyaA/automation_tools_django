8db8ff1f72294bf66dfb4e93d50bfc609928dd86
YIlyaA <anukovich@gmail.com>
2025-02-09 17:50:14 +0100

    created stockData model

--- stockannalysis/admin.py
+++ stockannalysis/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Stock
+from .models import Stock, StockData
 
 class StockAdmin(admin.ModelAdmin):
     list_display = ["name", "symbol", "sector", "exchange", "country"]
     search_fields = ["name", "symbol", "exchange"]
 
 admin.site.register(Stock, StockAdmin)
+admin.site.register(StockData)
--- stockannalysis/models.py
+++ stockannalysis/models.py
@@ -9,3 +9,19 @@ class Stock(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class StockData(models.Model):
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    current_price = models.CharField(max_length=255, null=True, blank=True)
+    price_changed = models.CharField(max_length=255, null=True, blank=True)
+    percentage_change = models.CharField(max_length=255, null=True, blank=True)
+    previous_close = models.CharField(max_length=255, null=True, blank=True)
+    week_52_high = models.CharField(max_length=255, null=True, blank=True)
+    week_52_low = models.CharField(max_length=255, null=True, blank=True)
+    market_cap = models.CharField(max_length=255, null=True, blank=True)
+    pe_ratio = models.CharField(max_length=255, null=True, blank=True)
+    dividend_yield = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.stock} - {self.current_price}"
